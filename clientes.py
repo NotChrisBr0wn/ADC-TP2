5d0c28a09eaf1653980ab26abfa4177ed89a2ecf
Rafael Santos <a84788@ualg.pt>
2023-11-30 16:57:10 +0000

    Adicionamos a função para imprimir a lista de clientes.

--- clientes.py
+++ clientes.py
@@ -19,3 +19,10 @@ def cria_novo_cliente():
     # ...
 
     pass
+
+def imprime_lista_de_clientes(lista_de_clientes):
+    """TODO: documentação"""
+
+    #TODO: Implementar esta função
+    # ...
+    pass
