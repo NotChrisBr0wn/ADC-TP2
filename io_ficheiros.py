4130465fc0e5acdbf2133330674d2e11fdb7850b
Vasco Vieira <a81602@ualg.pt>
2023-12-14 14:45:11 +0000

    adicionamos a funcao de carregar as listas dos ficheiros

--- io_ficheiros.py
+++ io_ficheiros.py
@@ -6,6 +6,7 @@ from faturas import nome_ficheiro_lista_de_faturas
 from veiculos import nome_ficheiro_lista_de_veiculos
 
 
+
 # TODO: Copie para aqui o código de cada uma das funções nos
 # ficheiros com o nome io_ficheiros*.py e faça um commit de cada vez
 # Quando este ficheiro estiver completo com todas as suas funções,
@@ -13,3 +14,12 @@ from veiculos import nome_ficheiro_lista_de_veiculos
 # todos os outros ficheiros io_ficheiros-*.py, e inclusive estes comentários
 
 # ...
+def carrega_as_listas_dos_ficheiros():
+    """TODO: documentação"""
+
+    lista_de_veiculos = le_de_ficheiro(nome_ficheiro_lista_de_veiculos)
+    lista_de_clientes = le_de_ficheiro(nome_ficheiro_lista_de_clientes)
+    lista_de_faturas = le_de_ficheiro(nome_ficheiro_lista_de_faturas)
+
+    return  lista_de_veiculos, lista_de_clientes, lista_de_faturas
+
